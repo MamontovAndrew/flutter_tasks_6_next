3a072964f95acfc695a785e05c3e29122cb75690
ai_mamontov <ai.mamontov@rusagro.tech>
2024-11-04 21:24:07 +0300

    fixed some bugs

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -20,9 +20,9 @@ class CartScreen extends StatefulWidget {
 
 class _CartScreenState extends State<CartScreen> {
   double get totalPrice => widget.cartItems.fold(
-    0,
+        0,
         (sum, item) => sum + item.product.price * item.quantity,
-  );
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -36,78 +36,78 @@ class _CartScreenState extends State<CartScreen> {
             child: widget.cartItems.isEmpty
                 ? Center(child: Text('Корзина пуста'))
                 : ListView.builder(
-              itemCount: widget.cartItems.length,
-              itemBuilder: (context, index) {
-                final cartItem = widget.cartItems[index];
-                return Slidable(
-                  endActionPane: ActionPane(
-                    motion: DrawerMotion(),
-                    extentRatio: 0.25,
-                    children: [
-                      SlidableAction(
-                        onPressed: (context) {
-                          widget.removeFromCart(cartItem.product);
-                          setState(() {});
-                        },
-                        backgroundColor: Colors.red,
-                        foregroundColor: Colors.white,
-                        icon: Icons.delete,
-                        label: 'Удалить',
-                      ),
-                    ],
-                  ),
-                  child: ListTile(
-                    leading: Image.network(
-                      cartItem.product.imageUrl,
-                      width: 50,
-                      height: 50,
-                      fit: BoxFit.cover,
-                    ),
-                    title: Text(
-                      cartItem.product.name,
-                      style: TextStyle(fontSize: 14), // Уменьшенный размер шрифта
-                    ),
-                    subtitle: Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            int newQuantity = cartItem.quantity - 1;
-                            widget.updateCartItemQuantity(
-                                cartItem.product, newQuantity);
-                            setState(() {});
-                          },
-                          icon: Icon(Icons.remove),
+                    itemCount: widget.cartItems.length,
+                    itemBuilder: (context, index) {
+                      final cartItem = widget.cartItems[index];
+                      return Slidable(
+                        endActionPane: ActionPane(
+                          motion: DrawerMotion(),
+                          extentRatio: 0.25,
+                          children: [
+                            SlidableAction(
+                              onPressed: (context) {
+                                widget.removeFromCart(cartItem.product);
+                                setState(() {});
+                              },
+                              backgroundColor: Colors.red,
+                              foregroundColor: Colors.white,
+                              icon: Icons.delete,
+                              label: 'Удалить',
+                            ),
+                          ],
                         ),
-                        Text('${cartItem.quantity}'),
-                        IconButton(
-                          onPressed: () {
-                            int newQuantity = cartItem.quantity + 1;
-                            widget.updateCartItemQuantity(
-                                cartItem.product, newQuantity);
-                            setState(() {});
-                          },
-                          icon: Icon(Icons.add),
-                        ),
-                      ],
-                    ),
-                    trailing: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          '${(cartItem.product.price * cartItem.quantity).toStringAsFixed(0)} ₽',
-                          style: TextStyle(
-                            fontSize: 16, // Увеличенный размер шрифта
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green,
+                        child: ListTile(
+                          leading: Image.network(
+                            cartItem.product.imageUrl,
+                            width: 50,
+                            height: 50,
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(
+                            cartItem.product.name,
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          subtitle: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  int newQuantity = cartItem.quantity - 1;
+                                  widget.updateCartItemQuantity(
+                                      cartItem.product, newQuantity);
+                                  setState(() {});
+                                },
+                                icon: Icon(Icons.remove),
+                              ),
+                              Text('${cartItem.quantity}'),
+                              IconButton(
+                                onPressed: () {
+                                  int newQuantity = cartItem.quantity + 1;
+                                  widget.updateCartItemQuantity(
+                                      cartItem.product, newQuantity);
+                                  setState(() {});
+                                },
+                                icon: Icon(Icons.add),
+                              ),
+                            ],
+                          ),
+                          trailing: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                '${(cartItem.product.price * cartItem.quantity).toStringAsFixed(0)} ₽',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
@@ -117,9 +117,7 @@ class _CartScreenState extends State<CartScreen> {
             ),
           ),
           ElevatedButton(
-            onPressed: () {
-              // Реализуйте функционал покупки
-            },
+            onPressed: () {},
             child: Text('Купить'),
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.green,
